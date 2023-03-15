60b42ce363c34e6ff7522624e8c1afbaf1e9cd8c
tinosnegocios <rodolfo0ti@gmail.com>
2023-03-14 22:05:29 -0300

    Creating context fot Post

---
+++ caixa-igreja/Registrations/ChurchCashFlow/Data/Context/PostContext.cs
@@ -0,0 +1,38 @@
+﻿using DataModelChurchCashFlow.Context.Interface;
+using DataModelChurchCashFlow.Models.Entities;
+
+namespace ChurchCashFlow.Data.Context
+{
+    public class PostContext : IPostContext
+    {
+        public Task Delete(Post Post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Post>? GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetOneNoTracking(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Post(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Put(Post editPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
