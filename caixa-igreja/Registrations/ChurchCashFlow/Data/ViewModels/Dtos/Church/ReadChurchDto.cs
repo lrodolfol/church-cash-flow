f95ba9819f0a5effcd8fda1610b665abd643eee0
tinosnegocios <rodolfo0ti@gmail.com>
2023-03-18 21:02:47 -0300

    Remove Id and Active from ReadDto User / Churhc

--- caixa-igreja/Registrations/ChurchCashFlow/Data/ViewModels/Dtos/Church/ReadChurchDto.cs
+++ caixa-igreja/Registrations/ChurchCashFlow/Data/ViewModels/Dtos/Church/ReadChurchDto.cs
@@ -1,8 +1,6 @@
 ﻿namespace ChurchCashFlow.Data.ViewModels.Dtos.Church;
 public class ReadChurchDto : ModelDto
 {
-    public int id { get; set; }
     public string Name { get; set; }
-    public bool Active { get; set; }
     public DataModelChurchCashFlow.Models.Entities.Address Address { get; set; }
 }
