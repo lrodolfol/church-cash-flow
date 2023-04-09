cba2950bb4c3d607c4fd477942b19e060fc0750b
tinosnegocios <rodolfo0ti@gmail.com>
2023-04-09 12:04:51 -0300

    fix: Fix name IHandler. is not a interface

---
+++ Registration/Registration.DomainCore/HandlerAbstraction/Handler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Registration.DomainCore.ViewModelAbstraction;
+
+namespace Registration.DomainCore.HandlerAbstraction;
+public abstract class Handler
+{
+    protected IMapper _mapper;
+    protected readonly CViewModel _viewModel;
+    protected int _statusCode;
+
+    protected Handler(IMapper mapper, CViewModel viewModel)
+    {
+        _mapper = mapper;
+        _viewModel = viewModel;
+    }
+
+    protected virtual bool ValidateCompetence(string competence)
+    {
+        DateTime t;
+        if (!DateTime.TryParse(competence.ToString(), out t))
+            return false;
+
+        return true;
+    }
+
+    
+
+}
