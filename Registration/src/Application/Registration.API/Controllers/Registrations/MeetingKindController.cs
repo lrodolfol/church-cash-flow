315f94df42136b346b845e01dae43f0a76c19f1c
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2023-06-14 20:08:42 -0300

    refactor: move the projects for correct path

---
+++ Registration/src/Application/Registration.API/Controllers/Registrations/MeetingKindController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.API.Extensions;
+using Registration.DomainCore.ViewModelAbstraction;
+using Microsoft.AspNetCore.Authorization;
+using Registration.Handlers.Handlers.Registrations;
+using Registration.Mapper.DTOs.Registration.MeetingKind;
+
+namespace Registration.API.Controllers.Registrations;
+public class MeetingKindController : ControllerBase
+{
+    public readonly MeetingKindHandler _handler;
+    private readonly CViewModel? _viewModel;
+
+    public MeetingKindController(MeetingKindHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT, M-TRS, L-TRS")]
+    [HttpGet("api/v1/meeting-kind")]
+    public async Task<IActionResult> GetAll([FromQuery] bool active = true)
+    {
+        var resultViewModel = await _handler.GetAll(active);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT, M-TRS, L-TRS")]
+    [HttpGet("api/v1/meeting-kind/{id:int}")]
+    public async Task<IActionResult> GetOne([FromRoute] int id)
+    {
+        var resultViewModel = await _handler.GetOne(id);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT")]
+    [HttpPost("api/v1/meeting-kind")]
+    public async Task<IActionResult> Create([FromBody] EditMeetingKindDto meetingKindDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            _viewModel!.SetErrors(ModelState.GetErrors());
+            return BadRequest(_viewModel);
+        }
+
+        var resultViewModel = await _handler.Create(meetingKindDto);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT")]
+    [HttpDelete("/api/v1/meeting-kind/{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var resultViewModel = await _handler.Delete(id);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+}
---
+++ Registration/src/Application/Registration.API/Controllers/Registrations/OutFlowController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.API.Extensions;
+using Registration.DomainCore.ViewModelAbstraction;
+using Microsoft.AspNetCore.Authorization;
+using Registration.Handlers.Handlers.Registrations;
+using Registration.Mapper.DTOs.Registration.OutFlow;
+
+namespace Registration.API.Controllers.Registrations;
+
+[ApiController]
+public class OutFlowController : ControllerBase
+{
+    private readonly OutFlowHanler _handler;
+    private readonly CViewModel? _viewModel;
+
+    public OutFlowController(OutFlowHanler handler)
+    {
+        _handler = handler;
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT, M-TRS, L-TRS")]
+    [HttpGet("api/v1/out-flow/all/{churchId:int}")]
+    public async Task<IActionResult> GetAll([FromRoute] int churchId, [FromQuery] bool active = true)
+    {
+        var resultViewModel = await _handler.GetAll(churchId, active);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "L-SCT, M-SCT, M-TRS, L-TRS")]
+    [HttpGet("api/v1/out-flow/{id:int}")]
+    public async Task<IActionResult> GetOne([FromRoute] int id)
+    {
+        var resultViewModel = await _handler.GetOne(id);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "M-TRS, L-TRS")]
+    [HttpPost("api/v1/out-flow")]
+    public async Task<IActionResult> Create([FromBody] EditOutFlowDto outFlowEditDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            _viewModel!.SetErrors(ModelState.GetErrors());
+            return BadRequest(_viewModel);
+        }
+
+        var resultViewModel = await _handler.Create(outFlowEditDto);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "M-TRS, L-TRS")]
+    [HttpPut("api/v1/out-flow/{id:int}")]
+    public async Task<IActionResult> Update([FromBody] EditOutFlowDto userEditDto, int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            _viewModel!.SetErrors(ModelState.GetErrors());
+            return BadRequest(_viewModel);
+        }
+
+        var resultViewModel = await _handler.Update(userEditDto, id);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+
+    [Authorize(Roles = "M-TRS, L-TRS")]
+    [HttpDelete("/api/v1/out-flow/{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var resultViewModel = await _handler.Delete(id);
+
+        return StatusCode(_handler.GetStatusCode(), resultViewModel);
+    }
+}
