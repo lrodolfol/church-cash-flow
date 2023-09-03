79827ac42412b549a7d838ef2645ecdd1d07b8a9
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2023-09-03 17:24:32 -0300

    chore: tdd for offering kind

---
+++ Registration/src/Test/HandlerTest/Builders/Entities/OfferingKindTest.cs
@@ -0,0 +1,23 @@
+﻿using HandlersTest.Builders.Dtos;
+using Registration.DomainBase.Entities.Registrations;
+using Registration.Mapper.DTOs.Registration.OfferingKind;
+
+namespace HandlersTest.Builders.Entities;
+
+public class OfferingKindTest
+{
+    public static OfferingKind ValidObjectOne()
+    {
+        var dto = EditOfferingKindDtoTest.ValidObjectOne();
+        return ConvertTo(dto);
+    }
+
+    public static OfferingKind ValidObjectTwo()
+    {
+        var dto = EditOfferingKindDtoTest.ValidObjectTwo();
+        return ConvertTo(dto);
+    }
+
+    private static OfferingKind ConvertTo(EditOfferingKindDto dto) =>
+        new OfferingKind(dto.Id, dto.Name, dto.Description);
+}
