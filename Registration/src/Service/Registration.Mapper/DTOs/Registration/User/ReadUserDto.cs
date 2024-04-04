a4722928194184a393a2ea86b4d769981189bd28
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-04 18:36:12 -0300

    feat: return churchId for get users

--- Registration/src/Service/Registration.Mapper/DTOs/Registration/User/ReadUserDto.cs
+++ Registration/src/Service/Registration.Mapper/DTOs/Registration/User/ReadUserDto.cs
@@ -6,5 +6,6 @@ public class ReadUserDto : ModelDto
     public string Code { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Church { get; set; } = string.Empty;
+    public int ChurchId { get; set; }
     public List<string> UserRoles { get; set; }
 }
