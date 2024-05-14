14bcfec4d80a6155a20bd2656832fede7ea6d9f1
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-05-13 21:52:33 -0300

    feat: create mongo command repository

---
+++ Registration/src/Repository/Registration.Repository/Repository/Mongo/MongoCommandRepository.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using Registration.DomainBase.Entities.Registrations;
+
+namespace Registration.Repository.Repository.Mongo;
+public class MongoCommandRepository<T>
+{
+    private readonly IMongoDatabase _dataBase;
+    private readonly IMongoCollection<T> _collection;
+    protected string _collectionName { get; set; }
+
+    public MongoCommandRepository(IMongoDatabase context, string collectionName)
+    {
+        _dataBase = context;
+        _collectionName = collectionName;
+        _collection = _dataBase.GetCollection<T>(_collectionName);
+    }
+    
+    public async Task Create(T user)
+    {
+        await _collection.InsertOneAsync(user);
+    }
+}
