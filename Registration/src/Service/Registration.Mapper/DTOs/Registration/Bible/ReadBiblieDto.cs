70e1298a7d75164e4ed5da5b6c66acddec387354
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-12-04 22:20:04 -0300

    read dto

---
+++ Registration/src/Service/Registration.Mapper/DTOs/Registration/Bible/ReadBiblieDto.cs
@@ -0,0 +1,19 @@
+﻿using Registration.DomainBase.Entities.Registrations.Bible;
+
+namespace Registration.Mapper.DTOs.Registration.Bible;
+public class ReadBiblieDto
+{
+    public string Book { get; set; } = null!;
+    public int Chapter { get; set; }
+    public List<Verse>? Verses { get; set; }
+
+
+    public static implicit operator ReadBiblieDto(GetBibleDto entitie)
+    {
+        return new ReadBiblieDto
+        {
+            Book = entitie.Book,
+            Chapter = entitie.Chapter
+        };
+    }
+}
