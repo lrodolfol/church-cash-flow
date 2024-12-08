a354da72b66847c88781132519e08c225de8cc70
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-12-08 10:52:40 -0300

    Update for receive delete month cloesed message

--- Serveless/MonthlyClosing/src/MonthlyClosing/Function.cs
+++ Serveless/MonthlyClosing/src/MonthlyClosing/Function.cs
@@ -16,6 +16,8 @@ namespace MonthlyClosing;
 public class Function
 {
     IAmazonS3 S3Client { get; set; }
+    string BuckerName { get; set; }
+
     public Function(IAmazonS3 s3Client)
     {
         this.S3Client = s3Client;
@@ -23,6 +25,13 @@ public class Function
     public Function()
     {
         S3Client = new AmazonS3Client();
+
+        if (string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "PROD", StringComparison.Ordinal))
+            BuckerName = "churchmanager-prod";
+        else if (string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "UAT", StringComparison.Ordinal))
+            BuckerName = "churchmanager-uat";
+        else
+            BuckerName = "churchmanager-dev";
     }
 
     public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
@@ -36,20 +45,72 @@ public class Function
     private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
     {
         context.Logger.LogInformation($"Mensagem para processamento: {message.Body}");
-        var JsonSerializerOptions = new JsonSerializerOptions
+
+        if (message.EventSource == "aws:sqs" && message.EventSourceArn.Contains("monthly-closing"))
         {
-            PropertyNameCaseInsensitive = true
-        };
+            await PostMonthClosedAsync(message, context);
+        }
+        else if (message.EventSource == "aws:sqs" && message.EventSourceArn.Contains("monthly-opening"))
+        {
+            await DeleteMonthClosedAsyn(message, context);
+        }
+    }
+
+    private async Task PostMonthClosedAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
         try
         {
             var body = JsonSerializer.Deserialize<Model>(message.Body);
-            if(body == null)
+            if (body == null)
             {
                 context.Logger.LogError("Error to deserialize message body");
-                return;
+                throw new Exception("Error to deserialize message body");
             }
 
-            await CreatePDF(body, context);
+            await CreatePdfSendToS3Async(body, context);
+        }
+        catch (Exception ex)
+        {
+            context.Logger.LogError($"Error to process message body. Error: {ex.Message}");
+        }
+
+        await Task.CompletedTask;
+    }
+    private async Task CreatePdfSendToS3Async(Model cashFlowRequest, ILambdaContext context)
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        var document = new MonthClosedRequest(cashFlowRequest);
+        var file = document.GeneratePdf();
+        string key = $"month-work/{cashFlowRequest.ChurchId}.{cashFlowRequest.Competence}.pdf";
+
+        PutObjectRequest request = new PutObjectRequest
+        {
+            BucketName = BuckerName,
+            Key = key,
+            InputStream = new MemoryStream(file),
+            ContentType = "application/pdf"
+        };
+
+        PutObjectResponse response = await S3Client.PutObjectAsync(request);
+        if (response.HttpStatusCode != HttpStatusCode.OK)
+            context.Logger.LogError($"Fail to send file to S3 - key: {key}");
+        else
+            context.Logger.LogInformation("File sent to S3");
+    }
+
+    private async Task DeleteMonthClosedAsyn(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
+        try
+        {
+            var body = JsonSerializer.Deserialize<MonthOpenedRequest>(message.Body);
+            if (body == null)
+            {
+                context.Logger.LogError("Error to deserialize message body");
+                throw new Exception("Error to deserialize message body");
+            }
+
+            await DeletePdfFromS3Async(body, context);
         }
         catch (Exception ex)
         {
@@ -59,33 +120,19 @@ public class Function
         await Task.CompletedTask;
     }
 
-    private async Task CreatePDF(Model cashFlowRequest, ILambdaContext context)
+    private async Task DeletePdfFromS3Async(MonthOpenedRequest body, ILambdaContext context)
     {
-        QuestPDF.Settings.License = LicenseType.Community;
-        
-        var document = new InvoiceDocument(cashFlowRequest);
-        var file = document.GeneratePdf();
-        var bucketName = "";
-        
-        if(string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "PROD", StringComparison.Ordinal))
-            bucketName = "churchmanager-prod";
-        else if(string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "UAT", StringComparison.Ordinal))
-            bucketName = "churchmanager-uat";
-        else
-            bucketName = "churchmanager-dev";
-
-        PutObjectRequest request = new PutObjectRequest
+        string key = $"month-work/{body.ChurchId}.{body.Competence}.pdf";
+        DeleteObjectRequest request = new DeleteObjectRequest
         {
-            BucketName = bucketName,
-            Key = $"month-work/{cashFlowRequest.ChurchId}.{cashFlowRequest.Competence}.pdf",
-            InputStream = new MemoryStream(file),
-            ContentType = "application/pdf"
+            BucketName = BuckerName,
+            Key = key
         };
 
-        PutObjectResponse response = await S3Client.PutObjectAsync(request);
-        if (response.HttpStatusCode != HttpStatusCode.OK)
-            context.Logger.LogError("Fail to send file to S3");
+        DeleteObjectResponse response = await S3Client.DeleteObjectAsync(request);
+        if(response.HttpStatusCode != HttpStatusCode.NoContent)
+            context.Logger.LogError($"Fail to delete file from S3 - key: {key}-{response.HttpStatusCode}");
         else
-            context.Logger.LogInformation("File sent to S3");    
+            context.Logger.LogInformation("File deleted from S3");  
     }
 }
---
+++ Serveless/MonthlyClosing/src/MonthlyClosing/MonthOpenedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MonthlyClosing;
+public class MonthOpenedRequest
+{
+    public int ChurchId { get; set; }
+    public string ChurchName { get; set; } = null!;
+    public string Competence { get; set; } = null!;
+    public string OcurredOn { get; set; } = null!;
+}
