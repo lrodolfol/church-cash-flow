338badfb09973921232960062fafc07d6f8c798d
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-12-19 18:38:36 -0300

    shared project with initial entities builders

---
+++ Registration/src/Test/Unitest/Registration.UnitTest.Shared/Builders/Entities/MemberInBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Registration.DomainBase.Entities.Registrations;
+
+namespace Registration.UnitTest.Shared.Builders.Entities;
+internal class MemberInBuilder : BaseBuilderEntitie
+{
+    public MemberIn Get => GetValid();
+    private MemberIn GetValid() =>
+        new MemberIn(new ChurchBuilder().Get.Name!,
+            GetValidDescription(),
+            GetValidDescription(),
+            new MemberBuilder().Get.Id
+            );
+}
---
+++ Registration/src/Test/Unitest/Registration.UnitTest.Shared/Builders/Entities/MemberPostBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Registration.DomainBase.Entities.Registrations;
+
+namespace Registration.UnitTest.Shared.Builders.Entities;
+internal class MemberPostBuilder : BaseBuilderEntitie
+{
+    public MemberPost Get => GetValid();
+    private MemberPost GetValid() =>
+        new MemberPost(GetRandomInt(),
+            new PostBuilder().Get.Id
+            );
+}
