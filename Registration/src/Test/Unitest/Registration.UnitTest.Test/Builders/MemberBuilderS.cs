b36d9762924531851f41a4dc24d4432678a125c8
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-01-18 14:04:07 -0300

    member test

---
+++ Registration/src/Test/Unitest/Registration.UnitTest.Test/Builders/MemberBuilderS.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Registration.DomainBase.Entities.Registrations;
+using Registration.DomainCore.ContextAbstraction;
+using Registration.Mapper.DTOs.Registration.Member;
+using Registration.Mapper.Profiles;
+using Registration.Repository;
+using Registration.Repository.Repository.Registration;
+using Registration.Test.Shared.Builders.Mappers;
+using Registration.UnitTest.Shared.Builders.Mappers;
+
+namespace Registration.UnitTest.Test.Builders;
+public class MemberBuilders : BaseBuilder
+{
+    internal void Initialize()
+    {
+        BuilderDataContext();
+        BuilderRespository();
+        BuilderMapper();
+
+        ServiceProvider = ServiceCollection.BuildServiceProvider();
+    }
+    private ServiceCollection BuilderRespository()
+    {
+        ServiceCollection.AddScoped<IMemberRepository, MemberRepository>();
+
+        return ServiceCollection;
+    }
+    private ServiceCollection BuilderMapper()
+    {
+        ServiceCollection.AddSingleton(MemberMapperBuilder.Mapper());
+        ServiceCollection.AddAutoMapper(typeof(MemberProfile));
+
+        return ServiceCollection;
+    }
+    public IMemberRepository GetRepository()
+    {
+        var context = ServiceProvider.GetRequiredService<DataContext>();
+        context.Database.EnsureCreated();
+
+        IMemberRepository respository = ServiceProvider.GetRequiredService<IMemberRepository>();
+        return respository;
+    }
+
+    public Member GetValidEntitie()
+    {
+        return new Member(GetRandomInt(),
+            GetValidPersonFullNameName(),
+            GetValidBeforeDate(),
+            GetValidBeforeDate(),
+            GetRandomInt(),
+            GetValidCode(),
+            GetValidNearDate()
+            );
+    }
+    public EditMemberDto GetValidEditDto() =>
+        GetMapper().Map<EditMemberDto>(GetValidEntitie());
+
+    public IMapper GetMapper() =>
+        BaseModelMapperBuilder<Member>.Mapper();
+}
---
+++ Registration/src/Test/Unitest/Registration.UnitTest.Test/MemberTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Registration.DomainCore.ContextAbstraction;
+using Registration.DomainCore.ViewModelAbstraction;
+using Registration.Handlers.Handlers.Registrations;
+using Registration.Mapper.DTOs.Registration.ChurchAddress;
+using Registration.Mapper.DTOs.Registration.Member;
+using Registration.UnitTest.Test.Builders;
+
+namespace Registration.UnitTest.Test;
+public class MemberTest : BaseUnitTest, IDisposable
+{
+    private readonly MemberBuilders _fixture;
+
+    public MemberTest(MemberBuilders fixture)
+    {
+        _fixture = fixture;
+        _fixture.Initialize();
+    }
+
+    [Fact(DisplayName = nameof(Create))]
+    [Trait("Domain", "Member - create")]
+    public async void Create()
+    {
+        IMemberRepository repository = _fixture.GetRepository();
+        IMapper mapper = _fixture.GetMapper();
+        EditMemberDto churchAddress = _fixture.GetValidEditDto();
+
+        MemberHandler hand = new MemberHandler(repository, mapper, _viewModel, operationsHandler, postHandler, churchHandler, memberBridgesHandler, _mockLogger.Object, configuration, memoryCache);
+        CViewModel handResult = await hand.Create(churchAddress);
+
+        Assert.NotNull(handResult);
+        Assert.NotNull(handResult.Data);
+        Assert.True(handResult.Errors.Count == 0);
+    }
+
+    public void Dispose() =>
+        _fixture.ClearContext();
+}
